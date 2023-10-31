c76114ab4b5be1ad9f6f1ec196f027987a59eb68
HugoSilva370 <hugo.oliveira.silva@devoteam.com>
2023-10-31 21:48:27 +0000

    add draw shapes

--- ar_paint.py
+++ ar_paint.py
@@ -30,6 +30,26 @@ def load_color_limits(json_file):
         color_limits=limits['limits']
     return color_limits
 
+def draw_circle(drawing_data):
+    if drawing_data['drawing']:
+        drawing_data['temp_img'] = drawing_data['img'].copy()
+        if (drawing_data['start_pos'] == (0, 0)): return
+        circle_radius = int(np.sqrt((drawing_data['start_pos'][0] - drawing_data['previous_x']) ** 2 + (drawing_data['start_pos'][1] - drawing_data['previous_y']) ** 2))
+        cv2.circle(drawing_data['temp_img'], (drawing_data['start_pos'][0], drawing_data['start_pos'][1]), circle_radius, drawing_data['color'], drawing_data['thickness'])
+
+def draw_square(drawing_data):
+    if drawing_data['drawing']:
+        drawing_data['temp_img'] = drawing_data['img'].copy()
+        if (drawing_data['start_pos'] == (0, 0)): return
+        cv2.rectangle(drawing_data['temp_img'], (drawing_data['start_pos'][0], drawing_data['start_pos'][1]), (drawing_data['previous_x'], drawing_data['previous_y']), drawing_data['color'], drawing_data['thickness'])
+
+def draw_ellipse(drawing_data):
+    if drawing_data['drawing']:
+        drawing_data['temp_img'] = drawing_data['img'].copy()
+        if (drawing_data['start_pos'] == (0, 0)): return
+        ellipse_axis = (int(np.sqrt((drawing_data['start_pos'][0] - drawing_data['previous_x']) ** 2 + (drawing_data['start_pos'][1] - drawing_data['previous_y']) ** 2)), 50)
+        cv2.ellipse(drawing_data['temp_img'], (drawing_data['start_pos'][0], drawing_data['start_pos'][1]), ellipse_axis, 0, 0, 360, drawing_data['color'], drawing_data['thickness'])
+
 def main():
     # -----------------------------------------------
     # Initialization
@@ -60,10 +80,9 @@ def main():
     default_img = copy.deepcopy(canvas)
 
     #....Image data storing...
-    drawing_data = {'img': canvas, 'pencil_down': False, 'previous_x': 0, 'previous_y': 0, 'color': (255, 255, 255), 'thickness': 5}
+    drawing_data = {'img': canvas, 'pencil_down': False, 'previous_x': 0, 'previous_y': 0, 'color': (255, 255, 255), 'thickness': 5, 'drawing': False, 'start_pos': (0, 0), 'temp_img': canvas.copy()}
 
     cv2.namedWindow("canvas")
-    cv2.setMouseCallback("canvas", partial(mouseCallback, drawing_data=drawing_data))
 
     #....Shakedown initialization....
     prev_center = None
@@ -103,28 +122,34 @@ def main():
             largest_object_idx = 1 + stats[1:, cv2.CC_STAT_AREA].argmax()   #Stores the biggest area
 
             largest_object_mask = (labels == largest_object_idx).astype(np.uint8)*255
-            contours, _ = cv2.findContours(largest_object_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             largest_object_mask = cv2.cvtColor(largest_object_mask, cv2.COLOR_GRAY2BGR)
 
             frame_with_highlight = cv2.addWeighted(frame, 1, largest_object_mask, 0.5, 0)
 						
-            if len(contours) > 0:
-                largest_contour = max(contours, key = cv2.contourArea)
-                M = cv2.moments(largest_contour)
-                if M['m00'] != 0:
-                    center_x = int(M['m10'] / M['m00'])
-                    center_y = int(M['m01'] / M['m00'])
-                    current_center = (center_x, center_y)
+            if len(centroids) > 0:
+                current_center = centroids[largest_object_idx]
+                center_x = int(current_center[0])
+                center_y = int(current_center[1])
+                cv2.line(frame_with_highlight, (center_x-5,center_y-5), (center_x+5,center_y+5), (0, 0, 255), 2)
+                cv2.line(frame_with_highlight, (center_x+5,center_y-5), (center_x-5,center_y+5), (0, 0, 255), 2)
+                if use_shake == False:
+                    if drawing_mode == 'Line':
+                        cv2.line(drawing_data['img'], (drawing_data['previous_x'], drawing_data['previous_y']), (center_x, center_y), drawing_data['color'], drawing_data['thickness'])
 
-                    cv2.line(frame_with_highlight, (center_x-5,center_y-5), (center_x+5,center_y+5), (0, 0, 255), 2)
-                    cv2.line(frame_with_highlight, (center_x+5,center_y-5), (center_x-5,center_y+5), (0, 0, 255), 2)
-                    
-                    if use_shake == False:
-                        if drawing_mode == 'Line':
-                            cv2.line(drawing_data['img'], (drawing_data['previous_x'], drawing_data['previous_y']), current_center, drawing_data['color'], drawing_data['thickness'])
+                    if drawing_mode == 'Circle':
+                        if drawing_data['drawing'] == False:
+                            drawing_data['start_pos'] = (center_x, center_y)
+                        draw_circle(drawing_data)
                         
-                        drawing_data['previous_x'] = center_x
-                        drawing_data['previous_y'] = center_y
+                    if drawing_mode == 'Square':
+                        if drawing_data['drawing'] == False:
+                            drawing_data['start_pos'] = (center_x, center_y)
+                        draw_square(drawing_data)
+                        
+                    if drawing_mode == 'Ellipse':
+                        if drawing_data['drawing'] == False:
+                            drawing_data['start_pos'] = (center_x, center_y)
+                        draw_ellipse(drawing_data)
                 else:
                     current_center = None
                 
@@ -140,6 +165,8 @@ def main():
                     
                     prev_center = current_center
             
+                drawing_data['previous_x'] = center_x
+                drawing_data['previous_y'] = center_y
             cv2.imshow('Biggest Area Highlight', frame_with_highlight)
         else:
             cv2.imshow('Biggest Area Highlight', frame)
@@ -149,8 +176,11 @@ def main():
         if args['use_camera_stream']:
             camera_and_canvas = cv2.addWeighted(frame, 1, drawing_data['img'], 1, 0)
             cv2.imshow('canvas', camera_and_canvas)
-        else: 
-            cv2.imshow('canvas', drawing_data['img'])
+        else:
+            if drawing_data['drawing']:
+                cv2.imshow('canvas', drawing_data['temp_img'])
+            else:
+                cv2.imshow('canvas', drawing_data['img'])
 
         #....Key awaiting....
         key = cv2.waitKey(50)
@@ -188,14 +218,21 @@ def main():
         elif key == ord('s'):
             print('Changed mode to square')
             drawing_mode = 'Square'
+            drawing_data['drawing'] = True
 
         elif key == ord('e'):
-            print('Changed to elipse mode')
-            drawing_mode = 'Elipse'
+            print('Changed to ellipse mode')
+            drawing_mode = 'Ellipse'
+            drawing_data['drawing'] = True
 
         elif key == ord('o'):
             print('Changed to circle mode')
             drawing_mode = 'Circle'
+            drawing_data['drawing'] = True
+
+        elif key == ord('l'):
+            print('Changed to line mode')
+            drawing_mode = 'Line'
 
         elif key == ord('c'):
             print('Pressed C button')
@@ -205,6 +242,12 @@ def main():
             print('Pressed W button')
             date = datetime.datetime.now().strftime('%a_%b_%d_%H:%M:%S_%Y')
             cv2.imwrite(f'./drawing_{date}.png', drawing_data['img'])
+        
+        else:
+            if drawing_data['drawing']:
+                drawing_data['img'] = drawing_data['temp_img'].copy()
+            drawing_data['drawing'] = False
+            drawing_data['start_pos'] = (0, 0)
 
     # -----------------------------------------------
     # Termination
