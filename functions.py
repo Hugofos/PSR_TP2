cc84c98cf8753a031d12516015d9d5da2d063756
HugoSilva370 <hugo.oliveira.silva@devoteam.com>
2023-11-01 19:23:36 +0000

    image segmentation complete

--- functions.py
+++ functions.py
@@ -58,22 +58,22 @@ def pressed_key(key, drawing_data, default_img):
     
     elif key == ord('s'):
         print('Changed mode to square')
-        drawing_mode = 'Square'
+        drawing_data['drawing_mode'] = 'Square'
         drawing_data['drawing'] = True
 
     elif key == ord('e'):
         print('Changed to ellipse mode')
-        drawing_mode = 'Ellipse'
+        drawing_data['drawing_mode'] = 'Ellipse'
         drawing_data['drawing'] = True
 
     elif key == ord('o'):
         print('Changed to circle mode')
-        drawing_mode = 'Circle'
+        drawing_data['drawing_mode'] = 'Circle'
         drawing_data['drawing'] = True
 
     elif key == ord('l'):
         print('Changed to line mode')
-        drawing_mode = 'Line'
+        drawing_data['drawing_mode'] = 'Line'
 
     elif key == ord('c'):
         print('Pressed C button')
@@ -85,43 +85,92 @@ def pressed_key(key, drawing_data, default_img):
         cv2.imwrite(f'./drawing_{date}.png', drawing_data['img'])
 
     elif key == ord('m'):
-        numbered_painting()
+        segment_image(drawing_data)
     
     else:
         if drawing_data['drawing']:
             drawing_data['img'] = drawing_data['temp_img'].copy()
         drawing_data['drawing'] = False
         drawing_data['start_pos'] = (0, 0)
+    
+def segment_image(drawing_data):
+    # Create a blank white canvas
+    width, height = 512, 512
+    img = np.ones((height, width, 3), np.uint8) * 255
+    num_lines = 3
 
-def numbered_painting():
-    num_sections = 8
-    width = 512
-    height = 512
+    # Draw 3 vertical polylines
+    for i in range(num_lines):
+        points = []
 
-    img = np.zeros((height, width, 3), np.uint8)
+        # First point: y = 0
+        x1 = (i + 1) * (width // (num_lines + 1))
+        y = 0
+        points.append((x1, y))
 
-    # Define the center of the image
-    center_x, center_y = width // 2, height // 2
+        # Second point: limited by 10 pixels on x, y at the bottom
+        x2 = x1 + random.randint(-50, 50)
+        y = height / 2
+        points.append((x2, y))
 
-    # List to store the vertices of each section
-    section_vertices = []
+        # Third point: limited by 10 pixels on x, y at the bottom
+        x = x2 + random.randint(-50, 50)
+        y = height
+        points.append((x, y))
 
-    for _ in range(num_sections):
-        num_vertices = random.randint(3, 8) # Randomly select the number of vertices for each section
-        section = []
-        for _ in range(num_vertices):
-            x = random.randint(0, width)
-            y = random.randint(0, height)
-            section.append((x, y))
-        section_vertices.append(section)
+        points = np.array(points, np.int32)
+        points = points.reshape((-1, 1, 2))
 
-    # Draw sections and labels
-    for i, vertices in enumerate(section_vertices):
-        vertices = np.array(vertices)
-        cv2.fillPoly(img, [vertices], (255, 255, 255))
-        for j in range(vertices.shape[0]):
-            cv2.line(img, tuple(vertices[j]), tuple(vertices[(j + 1) % vertices.shape[0]]), (0, 0, 0), 2) # Draw black lines
-        label_x, label_y = np.mean(vertices, axis = 0).astype(int)
-        cv2.putText(img, str(i), (label_x, label_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        color = (0, 0, 0)
 
-    cv2.imshow('Random shapes', img)
+        cv2.polylines(img, [points], isClosed=False, color=color, thickness=2)
+
+    # Draw 3 horizontal polylines
+    for j in range(num_lines):
+        points = []
+
+        # First point: x = 0
+        x = 0
+        y1 = (j + 1) * (height // (num_lines + 1))
+        points.append((x, y1))
+
+        # Second point: limited by 10 pixels on y, x at the right end
+        x = width / 2
+        y2 = y1 + random.randint(-50, 50)
+        points.append((x, y2))
+
+        # Third point: limited by 10 pixels on y, x at the right end
+        x = width
+        y = y2 + random.randint(-50, 50)
+        points.append((x, y))
+
+        points = np.array(points, np.int32)
+        points = points.reshape((-1, 1, 2))
+
+        color = (0, 0, 0)
+
+        cv2.polylines(img, [points], isClosed=False, color=color, thickness=2)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply thresholding to separate regions
+    ret, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+
+    # Find contours of connected components
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Assign random numbers to sections and label them at the center
+    for i, contour in enumerate(contours):
+        section_number = random.randint(1, 3)
+        M = cv2.moments(contour)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cv2.putText(img, str(section_number), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
+    # Draw contours on the image for visualization
+    cv2.drawContours(img, contours, -1, (0, 0, 0), 2)
+
+    # Display the image
+    drawing_data['img'] = img.copy()
